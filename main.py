8c92d410e351b25baf67589e52942197b2301f51
Gabriel Aubin-Moreau <96608418+AccidentalGenius101@users.noreply.github.com>
2023-02-23 23:28:31 -0500

    23-02-2023

--- main.py
+++ main.py
@@ -4,37 +4,65 @@ from river import metrics
 from river import evaluate
 import pandas as pd
 import time
+from sklearn.model_selection import train_test_split
 from sklearn import tree as treeSK
+import graphviz
+from sklearn.model_selection import cross_validate
 
-df = pd.read_csv("data/cleaned_speeddating.csv")
+# -------Evaluating DT tree-------
+
+# Data for DT
+dfDT = pd.read_csv("data/cleaned_speeddatingDT.csv").drop(columns="Unnamed: 0")
+XDT = dfDT.iloc[:, :-3]
+yDT = dfDT.iloc[:, -1]
+X_train, X_test, y_train, y_test = train_test_split(XDT, yDT, test_size=0.30, random_state=42)
+
+# DT Model
+start = time.time()
+dt = treeSK.DecisionTreeClassifier()
+dt = dt.fit(X_train, y_train)
+
+# Visualising DT tree
+dot_data = treeSK.export_graphviz(dt, out_file=None,
+                                  feature_names=XDT.columns,
+                                  filled=True, rounded=True,
+                                  special_characters=True)
+graph = graphviz.Source(dot_data)
+graph.render("DT")
+
+# Evaluating model
+scoring = ['accuracy', 'precision', 'f1', 'recall', 'roc_auc']
+dt_results = cross_validate(dt, X_test, y_test, scoring=scoring)
+print(dt_results)
+
+end = time.time()
+print("Time for DT: " + str(end - start))
+
+# -------Evaluating incremental tree-------
+
+# Data Stream for EFDT, VFDT and CVFDT
+df = pd.read_csv("data/cleaned_speeddating.csv").drop(columns="Unnamed: 0")
 X = df.iloc[:, :-3]
 y = df.iloc[:, -1]
 df = stream.iter_pandas(X, y)
 
+# Différent modèles
 efdt = tree.ExtremelyFastDecisionTreeClassifier()
 vfdt = tree.HoeffdingTreeClassifier()
 cvfdt = tree.HoeffdingAdaptiveTreeClassifier()
-dt = treeSK.DecisionTreeClassifier()
+modelList = [efdt, vfdt, cvfdt]
 
-
-
+# Différentes métriques
 accuracy = metrics.Accuracy()
 precision = metrics.Precision()
 recall = metrics.Recall()
-mcc = metrics.MCC()
 f1 = metrics.F1()
 rocauc = metrics.ROCAUC()
-confusionmatrix = metrics.ConfusionMatrix()
-crossentropy = metrics.CrossEntropy()
-logloss = metrics.LogLoss()
+metricsList = [accuracy, precision, recall, f1, rocauc]
 
-metricsList = [accuracy, precision, recall, mcc, f1, rocauc, confusionmatrix, crossentropy, logloss]
-
-dt = dt.fit(X, y)
-treeSK.plot_tree(dt)
-
-evaluate.progressive_val_score(dataset=df, model=efdt, metric=accuracy, print_every=200)
-t
-
-
-
+for model in modelList:
+    for metric in metricsList:
+        evaluate.progressive_val_score(dataset=df, model=model, metric=metric, print_every=1000, show_time=True,
+                                       show_memory=True)
+        evaluate.reset()
+        print("Model: " + str(model))
--- preparation.py
+++ preparation.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("data/speeddating.csv")
 
@@ -25,5 +26,25 @@ for column in df:
             df.at[i, str(column)] = x
             i += 1
 
+# On enlève les colonnes ayant plus de 400 valeurs manquantes
+dfDT = df.copy()
+missing_val = dfDT.isnull().sum().sort_values(ascending=False)
+for x in missing_val:
+    if x >= 400:
+        column = missing_val[missing_val == x].index[0]
+        dfDT = dfDT.drop(columns=[str(column)])
+print(dfDT.isnull().sum().sort_values(ascending=False))
+
+# On enlève les observations ayant des valeurs manquantes
+dfDT = dfDT.dropna()
+
+# Vérification des valeurs aberrantes
+df_num = dfDT.select_dtypes(exclude='object')
+df_num.boxplot()
+# plt.show()
+
 # Dataframe convertit en csv
+# Ensemble de données pour les arbres incrémentals
 df.to_csv("data/cleaned_speeddating.csv")
+# Ensemble de données pour l'arbre normal
+dfDT.to_csv("data/cleaned_speeddatingDT.csv")
